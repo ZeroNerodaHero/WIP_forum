56b9185431382e837490cb31deec8cfd2eb53534
billy <billyz404meter@gmail.com>
2021-12-14 17:57:41 -0800

    Login way revealed

---
+++ adminPower/commWserver_example.php
@@ -0,0 +1,12 @@
+<?php
+    //admin logins
+    $admin_ppassword="xxx";
+    $mod_ppassword="xxx";
+    $servername = "xxxx";
+    $user = "xxx";
+    $pass = "xxx";
+
+    //site settings
+    $maxThreads = 15;
+    $totalBury = 5;
+?>
