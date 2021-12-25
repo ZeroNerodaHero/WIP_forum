f672f7549cbb169c7c68d1c52cb97db35591ee14
billy <billyz404meter@gmail.com>
2021-12-24 17:24:02 -0800

    Better Nav

--- php/generateNav.php
+++ php/generateNav.php
@@ -1,22 +1,34 @@
 <?php
     include_once("../adminPower/login.php");
+	$cPage = "news";
+	if(!empty($_GET["page"])) $cPage = $_GET["page"];
+
     $pagetoGo = "?page=";
-    echo "<a href= \"".$pagetoGo. "news\">
-          <div id=NewsNavLink>[News]</div> </a>";
+
+	if($cPage == "news"){
+    	echo "<div class=NewsNavLink id=navActLink>[News]</div>";
+	} else{
+    	echo "<a href= \"".$pagetoGo. "news\">
+          <div class=NewsNavLink>[News]</div> </a>";
+	}
     echo "<div class=navCategory>Main Boards:</div>";
     $que = "SELECT boardName FROM boards WHERE typeOfBoard=\"main\"";
-    getBoard($que);
+    getBoard($que,$cPage);
     /* -----------------------------------------------------------*/
     echo "<div class=navCategory>Shit Boards:</div>";
     $que = "SELECT boardName FROM boards WHERE typeOfBoard=\"shit\"";
-    getBoard($que);
+    getBoard($que,$cPage);
 
-    function getBoard($boardquery){
+    function getBoard($boardquery,$cPage){
         global $conn,$pagetoGo;
         $res = $conn->query($boardquery);
         if($res->num_rows > 0){
             while($row = $res->fetch_assoc()){
                 $bname = $row["boardName"];
+				if($bname == $cPage){
+                	echo "<div class=navLink id=navActLink>[" . $bname.  "]</div>";
+					continue;
+				}
                 echo "<a href= \"".$pagetoGo. $bname."\">
                     <div class=navLink>[" . $bname.  "]</div> </a>";
             }
