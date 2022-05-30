05b6e580fcb62a684c6633c33d33c14bc3755ffa
eve <billyz404meter@gmail.com>
2022-05-29 17:27:32 -0700

    index update

--- php/index.php
+++ php/index.php
@@ -3,6 +3,7 @@
     <head>
         <link rel="stylesheet" href="../css/mainstyle.css">
         <link rel="stylesheet" href="../css/reuseStyle.css">
+        <link rel="stylesheet" href="../css/readerStyle.css">
         <link rel="icon" href="../res/icon/icon_0.png">
         <script type="text/javascript" src="../css/jscrap.js"></script>
         <script type="text/javascript" src="../css/cookieSetting.js"></script>
